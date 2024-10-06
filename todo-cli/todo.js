1568ebc963dbe404539265acda564f109a40a970
Piyush Dhyani <piyushdhyani7@gmail.com>
2024-10-06 17:18:06 +0530

    5 test case

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -81,21 +81,22 @@ const todoList = () => {
   // DO NOT CHANGE ANYTHING BELOW THIS LINE. #
   // ####################################### #
   
-  module.exports = todoList
   // const todos = todoList();
   
-  // const formattedDate = d => {
-  //   return d.toISOString().split("T")[0]
-  // }
+  const formattedDate = d => {
+    return d.toISOString().split("T")[0]
+  }
   
-  // var dateToday = new Date()
-  // const today = formattedDate(dateToday)
-  // const yesterday = formattedDate(
-  //   new Date(new Date().setDate(dateToday.getDate() - 1))
-  // )
-  // const tomorrow = formattedDate(
-  //   new Date(new Date().setDate(dateToday.getDate() + 1))
-  // )
+  var dateToday = new Date()
+  const today = formattedDate(dateToday)
+  const yesterday = formattedDate(
+    new Date(new Date().setDate(dateToday.getDate() - 1))
+  )
+  const tomorrow = formattedDate(
+    new Date(new Date().setDate(dateToday.getDate() + 1))
+  )
+  
+  module.exports = todoList
   
   // todos.add({ title: 'Submit assignment', dueDate: yesterday, completed: false })
   // todos.add({ title: 'Pay rent', dueDate: today, completed: true })
